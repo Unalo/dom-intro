0f2b13aa4c0155e83f540ab522cdb9c836b7006d
unalo <unalomtshokotsha@gmail.com>
2018-04-12 15:06:25 +0200

    first widget

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,13 +1,52 @@
+var roundedBillTotal = 0;
 //get a reference to the calculate button
-
+var calculateBtnElement = document.querySelector(".calculateBtn");
 //get a reference to the billTotal element
-
+var billTotalElement = document.querySelector(".billTotal");
 //get a reference to the billString
-
+var billStringElement = document.querySelector(".billString");
 //create the function that will be called when the calculate button is pressed
-//  * this function should read the string value entered - split it on a comma.
-//  * loop over all the entries in the the resulting list
-//  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
+function calculateBtnClicked () {
+  //  * this function should read the string value entered - split it on a comma.
+  var billStringFunction = billStringElement.value;
+  //console.log(billStringFunction);
+  var billItems = billStringFunction.split(",");
+//  console.log(billItems);
+  var billTotal = 0;
+  //  * loop over all the entries in the the resulting list
+  for (var i=0; i<billItems.length;i++) {
+    var billItem = billItems[i].trim();
+    console.log(billItem);
+    //  * check if it is a call or an sms and add the right amount to the overall total
+    if (billItem === "call") {
+      billTotal += 2.75;
+    }
+    else if (billItem === "sms") {
+      billTotal += 0.75;
+    }
+  }
+  //  * once done looping over all the entries - display the total onto the screen in the billTotal element
+  roundedBillTotal = billTotal.toFixed(2);
+  billTotalElement.innerHTML = roundedBillTotal;
+}
 
-//link the function to a click event on the calculate button
+function color() {
+  if (roundedBillTotal < 20) {
+      billTotalElement.classList.remove("warning");
+      billTotalElement.classList.remove("danger");
+  }
+   if (roundedBillTotal > 20 && 30 > roundedBillTotal ) {
+        billTotalElement.classList.remove("danger");
+        billTotalElement.classList.add("warning");
+  }
+  if (roundedBillTotal > 30 ) {
+    billTotalElement.classList.remove("warning");
+    billTotalElement.classList.add("danger");
+  }
+}
+calculateBtnElement.addEventListener('click',
+function () {
+  calculateBtnClicked();
+  color();
+}
+);
