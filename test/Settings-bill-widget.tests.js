7f34f08bd2e7d48164258a1a7c256b07049065fe
unalo <unalomtshokotsha@gmail.com>
2018-05-07 18:13:50 +0200

    test add nothing

--- test/Settings-bill-widget.tests.js
+++ test/Settings-bill-widget.tests.js
@@ -2,11 +2,10 @@ describe('The Bill with Settings function', function() {
   it("It should return the total for a call", function() {
 
     var settings = BillSettings();
-    settings.smsOne(2.50);
-
     settings.callOne(5.00);
+    settings.smsOne(2.50);
+    settings.warningOne(8.00);
     settings.criticalOne(10.00);
-    settings.warningOne(5.00);
 
     settings.calc('call');
 
@@ -16,42 +15,63 @@ describe('The Bill with Settings function', function() {
 
   });
 
-
-  it("It should return the total for an sms", function() {
+  it("It should return the total for sms", function() {
 
     var settings = BillSettings();
-    settings.sms(2.50);
+    settings.callOne(5.00);
+    settings.smsOne(2.50);
+    settings.warningOne(10.00);
+    settings.criticalOne(15.00);
 
-    settings.calls(5.00);
-    settings.critical(12.00);
-    settings.warning(6.00);
 
-    settings.sumBill('sms');
+    settings.calc('sms');
+    settings.calc('sms');
+    settings.calc('sms');
 
-    assert.equal(settings.sumCall(), 0.00);
-    assert.equal(settings.sumSms(), 2.50);
-    assert.equal(settings.sumTotal(), 2.50);
+    assert.equal(settings.callSumOne(), 0.00);
+    assert.equal(settings.smsSumOne(), 7.50);
+    assert.equal(settings.sumOne(), 7.50);
 
   });
 
   it("It should calculate total for multiple calls", function() {
 
     var settings = BillSettings();
-    settings.sms(2.50);
+    settings.callOne(5.00);
+    settings.smsOne(2.50);
+    settings.warningOne(7.00);
+    settings.criticalOne(15.00);
 
-    settings.calls(5.00);
-    settings.critical(15.00);
-    settings.warning(7.00);
+    settings.calc('call');
+    settings.calc('call');
+    settings.calc('sms');
+    settings.calc('call');
+    settings.calc('sms');
+    settings.calc('sms')
 
-    settings.sumBill('call');
-    settings.sumBill('call');
-    settings.sumBill('call');
-    settings.sumBill('call');
-
-    assert.equal(settings.sumCall(), 20.00);
-    assert.equal(settings.sumSms(), 0.00);
-    assert.equal(settings.sumTotal(), 20.00);
+    assert.equal(settings.callSumOne(), 15.00);
+    assert.equal(settings.smsSumOne(), 7.50);
+    assert.equal(settings.sumOne(), 22.50);
 
   });
 
+  it("It should return nothing if someone did not add anything", function() {
+    var settings = BillSettings();
+    settings.callOne(0.00);
+    settings.smsOne(0.00);
+    settings.warningOne(0.00);
+    settings.criticalOne(0.00);
+
+    settings.calc('');
+    settings.calc('');
+    settings.calc('');
+    settings.calc('');
+    settings.calc('');
+    settings.calc('')
+
+    assert.equal(settings.callSumOne(), 0.00);
+    assert.equal(settings.smsSumOne(), 0.00);
+    assert.equal(settings.sumOne(), 0.00);
+
+  });
 });
