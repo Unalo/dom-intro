0442cb8df97e74edc366cec494e974842c01eb9b
unalo <unalomtshokotsha@gmail.com>
2018-09-04 12:52:54 +0200

    now using handlebars for total side

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -4,6 +4,20 @@ var totalSmsTwo = document.querySelector(".smsTotalTwo");
 var sumTwo = document.querySelector(".totalTwo");
 var first = TextBill(); // factory function [instance]
 
+var totalTemplate = document.getElementById("totalsTwo");
+var totalsData = document.querySelector(".templateTotals").innerHTML;
+var templateData = Handlebars.compile(totalsData);
+
+window.addEventListener('load', function() {
+  var totals = {
+    callTotalTwo: "0.00",
+    smsTotalTwo: "0.00",
+    totalTwo: "0.00"
+  };
+  console.log("run template");
+  totalTemplate.innerHTML = templateData(totals);
+});
+
 function radioBill() {
   //input value
   var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
@@ -11,29 +25,37 @@ function radioBill() {
     var billItemType = checkedRadioBtn.value;
   }
   first.Maths(billItemType); // update calls and sms
-  totalCallTwo.innerHTML = first.Call().toFixed(2);
-  totalSmsTwo.innerHTML = first.Sms().toFixed(2);
-  sumTwo.innerHTML = first.Sum().toFixed(2);
+  var totals = {
+    callTotalTwo: first.Call(billItemType).toFixed(2),
+    smsTotalTwo: first.Sms(billItemType).toFixed(2),
+    totalTwo: first.Sum().toFixed(2),
+    color: first.Screen()
+  };
+  console.log("run template");
+  totalTemplate.innerHTML = templateData(totals);
+  // first.Maths(billItemType); // update calls and sms
+  // totalCallTwo.innerHTML = first.Call().toFixed(2);
+  // totalSmsTwo.innerHTML = first.Sms().toFixed(2);
+  // sumTwo.innerHTML = first.Sum().toFixed(2);
 }
+//
 // screen changes
-function totalColor() {
-  var sumTotal = first.Sum();
-  if (sumTotal < 30) {
-    sumTwo.classList.remove("danger");
-    sumTwo.classList.remove("warning");
-  }
-  if (sumTotal > 30 && 50 > sumTotal) {
-    sumTwo.classList.remove("danger");
-    sumTwo.classList.add("warning");
-  } else if (sumTotal > 50) {
-    sumTwo.classList.remove("warning");
-    sumTwo.classList.add("danger");
-  }
-}
+
+// function totalColor() {
+//   var sumTotal = first.Sum();
+//   if (sumTotal < 30) {
+//     sumTwo.classList.remove("danger");
+//     sumTwo.classList.remove("warning");
+//   }
+//   if (sumTotal > 30 && 50 > sumTotal) {
+//     sumTwo.classList.remove("danger");
+//     sumTwo.classList.add("warning");
+//   } else if (sumTotal > 50) {
+//     sumTwo.classList.remove("warning");
+//     sumTwo.classList.add("danger");
+//   }
+// }
 // add Event Listener [call my function]
-addBtn.addEventListener("click",
-  function() {
-    radioBill();
-    totalColor();
-  }
-);
+addBtn.addEventListener("click", function() {
+  radioBill();
+});
